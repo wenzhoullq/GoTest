4aef5bacb44b810d50498ca45a69bc03ed54f84a
liqiang.li <liqiang.li@liqiang.li>
2023-07-01 13:41:45 +0800

    新增手撕协程池

--- design/strategy/strategy.go
+++ design/strategy/strategy.go
@@ -1,43 +1,32 @@
 package main
 
-import "fmt"
-
-type Strategy interface {
+// 策略模式
+type Stragy interface {
 	Discount(float64) float64
 }
 
-type s1 struct{}
-
-func (s s1) Discount(price float64) float64 {
-	return price * 0.8
+type S1 struct {
 }
 
-type s2 struct{}
+func (s1 *S1) Discount(num float64) float64 {
+	return float64(num * 0.8)
+}
 
-func (s s2) Discount(price float64) float64 {
-	return price - 100
+type S2 struct {
+}
+
+func (s2 *S2) Discount(num float64) float64 {
+	return num - 100
 }
 
 type Buyer struct {
-	strategy Strategy
+	Stragy Stragy
 }
 
-func (b Buyer) SetStrategy(strategy Strategy) {
-	b.strategy = strategy
+func (Buyer *Buyer) SetStray(stragy Stragy) {
+	Buyer.Stragy = stragy
 }
 
-func (b Buyer) Buy(num float64) float64 {
-	return b.strategy.Discount(num)
-}
-
-// 抽象出策略类接口,然后写出具体的策略类;同时定义环境类,环境类有set方法和执行方法,根据不同的策略类作为参数最后的执行方法是不同的
-// strategy是策略类,buyer是环境类
-func main() {
-	s2 := s2{}
-	s1 := s1{}
-	buyer := Buyer{}
-	buyer.SetStrategy(s1)
-	fmt.Println(s1.Discount(100))
-	buyer.SetStrategy(s2)
-	fmt.Println(s2.Discount(100))
+func (Buyer *Buyer) Buy(num float64) float64 {
+	return Buyer.Stragy.Discount(num)
 }
---
+++ threadPool/threadPool.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"sync"
+)
+
+type Task struct {
+	task func()
+}
+
+func NewTask(t func()) Task {
+	return Task{
+		task: t,
+	}
+}
+
+type Pool struct {
+	lock       sync.Mutex
+	wg         sync.WaitGroup
+	workerNum  int
+	taskQueue  chan Task
+	entryQueue chan Task
+	ctx        context.Context
+	cancel     context.CancelFunc
+}
+
+func (p *Pool) AddTasks(tasks []Task) error {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	select {
+	case <-p.ctx.Done():
+		return errors.New("Pool has already close")
+	default:
+	}
+	p.wg.Add(len(tasks))
+	for i := 0; i < len(tasks); i++ {
+		p.entryQueue <- tasks[i]
+	}
+	return nil
+}
+
+func (p *Pool) AddTask() {
+	for {
+		select {
+		case t := <-p.entryQueue:
+			p.taskQueue <- t
+		case <-p.ctx.Done():
+			return
+		}
+	}
+}
+
+func NewPool(workerNum int) *Pool {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &Pool{
+		workerNum:  workerNum,
+		taskQueue:  make(chan Task),
+		entryQueue: make(chan Task),
+		ctx:        ctx,
+		cancel:     cancel,
+	}
+}
+
+func (p *Pool) Start() {
+	go p.AddTask()
+	for i := 0; i < p.workerNum; i++ {
+		go p.worker()
+	}
+}
+
+func (p *Pool) worker() {
+	for {
+		select {
+		case t := <-p.taskQueue:
+			t.task()
+			p.wg.Done()
+		case <-p.ctx.Done():
+			return
+		}
+	}
+}
+
+func (p *Pool) Close() {
+	p.wg.Wait()
+	//防止多并发环境下 一边执行stop,一边执行addTasks
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	p.cancel()
+}
+
+func main() {
+	pool := NewPool(3)
+	pool.Start()
+	tasks := make([]Task, 0)
+	for i := 0; i < 100; i++ {
+		task := NewTask(func() {
+			fmt.Println("test")
+		})
+		tasks = append(tasks, task)
+	}
+	err := pool.AddTasks(tasks)
+	if err != nil {
+		log.Println(err)
+	}
+	pool.Close()
+	err = pool.AddTasks(tasks)
+	if err != nil {
+		log.Println(err)
+	}
+}
